6c5b794ec635faa39510250f95bb7d260d6a4bf2
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2018-11-21 15:04:29 +0100

    Switched from Vue.js to vanilla php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include("includes/_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("includes/head.php");?>
+</head>
+<body>
+
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+    <h2>Welcome to my website!</h2>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
