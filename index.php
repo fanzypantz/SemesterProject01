9a4d0eb857159a7d153839915051c116fa57c04c
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2018-11-29 11:16:32 +0100

    added template inheritance addon

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<?php include("includes/_config.php");?>
-<!DOCTYPE html>
-<html>
-<head>
-    <?php include("includes/head.php");?>
-</head>
-<body>
+<?php include 'layout.php' ?>
 
-<?php include("includes/navigation.php");?>
-
-<div class="container" id="main-content">
-    <h2>Welcome to my website!</h2>
-</div>
-
-<?php include("includes/footer.php");?>
-
-</body>
-</html>
+<?php startblock('content') ?>
+    <h1>This is a title</h1>
+<?php endblock() ?>
---
+++ layout.php
@@ -0,0 +1,20 @@
+<?php require_once 'ti.php' ?>
+<?php include("includes/_config.php");?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <?php include("includes/head.php");?>
+
+    </head>
+    <body>
+
+        <?php include("includes/navigation.php");?>
+
+        <?php startblock('content') ?>
+        <?php endblock() ?>
+
+        <?php include("includes/footer.php");?>
+
+    </body>
+</html>
